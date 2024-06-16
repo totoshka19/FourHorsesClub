c893143c1b6e40ab981e8ff8101d9d2659d4939a
Anna Kapitanova <totoshechka19@gmail.com>
2024-06-16 15:44:57 +0300

    Бегущая строка

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Бегущая строка
+document.addEventListener("DOMContentLoaded", function() {
+  const tickerContainer = document.querySelector('.ticker__container');
+  const tickerTexts = document.querySelectorAll('.ticker__text');
+  let totalWidth = 0;
+
+  tickerTexts.forEach(text => {
+    totalWidth += text.offsetWidth;
+  });
+
+  // Добавляем немного дополнительного пространства, чтобы избежать скачков
+  totalWidth += 30 * (tickerTexts.length - 1);
+
+  tickerContainer.style.width = `${totalWidth}px`;
+
+  // Клонируем тексты для бесконечной прокрутки
+  tickerTexts.forEach(text => {
+    const clone = text.cloneNode(true);
+    tickerContainer.appendChild(clone);
+  });
+});
